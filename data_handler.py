a2ace8b9e5543e1bb4425e58cebb6cd5379956bf
Roy91 <rolonew@gmail.com>
2019-03-20 09:29:04 +0100

    question_add,id,submission_time

--- data_handler.py
+++ data_handler.py
@@ -1,4 +1,6 @@
 import connection
+import time
+from datetime import datetime
 
 question_header = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
@@ -12,4 +14,17 @@ def write_to_file(filename, dictionary):
 
 
 def get_id(filename):
-    return connection.get_csv_data(filename)[-1]['id']
+    existing_data = get_all_details(filename)
+    if len(existing_data) == 0:
+        return '1'
+    return str(int(existing_data[-1]['id']) + 1)
+
+
+def date_time():
+    # unix_time = int(time.time())
+    # return datetime.utcfromtimestamp(unix_time).strftime('%Y-%m-%d|%H:%M')
+    return int(time.time())
+
+
+def convert_unix_to_time(submission_time):
+    return datetime.utcfromtimestamp(submission_time).strftime('%Y-%m-%d|%H:%M')
--- server.py
+++ server.py
@@ -11,20 +11,56 @@ def list():
     return render_template('list.html', questions=questions)
 
 
-@app.route('/new_question', methods=['POST'])
+@app.route('/add-question', methods=['GET', 'POST'])
 def add_question():
-    question_details = {'id': request.form.get('id'),
-                        'submission_time': request.form.get('submission_time'),
-                        'view_number': request.form.get('view_number'),
-                        'vote_number': request.form.get('vote_number'),
-                        'title': request.form.get('title'),
-                        'message': request.form.get('message'),
-                        'image': request.form.get('image'),
-                        }
-    data_handler.write_to_file('question.csv', question_details)
-    labels = ['New Question', 'Post', 'Return']
-    max_id = int(data_handler.get_id('question.csv'))
-    return render_template('new_question.html', question=question_details, labels=labels, max_id=max_id)
+    if request.method == 'GET':
+        empty_questions = {}
+        return render_template('add-question.html',
+                               question=empty_questions,
+                               form_url=url_for('add_question'),
+                               page_title='Add Questions',
+                               button_title='Submit',
+                               button_page='Return'
+                               )
+
+    if request.method == 'POST':
+        question = {'id': data_handler.get_id('question.csv'),
+                    'submission_time': data_handler.date_time(),
+                    'view_number': request.form.get('view_number'),
+                    'vote_number': request.form.get('vote_number'),
+                    'title': request.form.get('title'),
+                    'message': request.form.get('message'),
+                    'image': request.form.get('image'),
+                    }
+        data_handler.write_to_file('question.csv', question)
+        return redirect('/')
+
+
+@app.route('/question/<question_id>', methods=['GET', 'POST'])
+def update_question(question_id):
+    if request.method == 'POST':
+        if request.form.get('id') != question_id:
+            raise ValueError('The received id is not valid!')
+        question = {'id': question_id,
+                    'submission_time': request.form.get('submission_time'),
+                    'view_number': request.form.get('view_number'),
+                    'vote_number': request.form.get('vote_number'),
+                    'title': request.form.get('title'),
+                    'message': request.form.get('message'),
+                    'image': request.form.get('image'),
+                    }
+        data_handler.update_question(question)
+        return redirect('/')
+
+    question = data_handler.get_user_question(question_id)
+
+    return render_template('add-question.html.html',
+                           question=question,
+                           form_url=url_for('update_question', question_id=question_id),
+                           page_title='Update Question',
+                           button_title='Update',
+                           button_page='Delete '
+                           )
 
 
 @app.route('/answers/<id>')
@@ -37,4 +73,4 @@ if __name__ == '__main__':
         host='0.0.0.0',
         port=8000,
         debug=True,
-)
+    )
